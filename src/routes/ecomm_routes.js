e42317d63ccc44a42586333a219d938a49d30278
tanishq14developer <tanishq.happy@gmail.com>
2023-07-28 02:07:13 +0530

    Feat:added authenticator

--- src/routes/ecomm_routes.js
+++ src/routes/ecomm_routes.js
@@ -7,6 +7,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 app.use(router);
 
+const {
+  authenticator,
+  basicApiKeyAuthenticator,
+} = require("../controller/authenticator.controller");
 const userController = require("../controller/user.controller");
 const productController = require("../controller/productDetails.controller");
 
@@ -14,7 +18,7 @@ router.post("/sendOtp", userController.sendOtp);
 router.post("/resendOTP", userController.resendOTP);
 router.post("/verifyOtp", userController.verifyOtp);
 router.post("/addProduct", productController.addProduct);
-router.get("/getProduct", productController.getProduct);
+router.get("/getProduct", authenticator, productController.getProduct);
 router.post("/updateProduct", productController.updateProduct);
 router.post("/deleteProduct", productController.deleteProduct);
 
