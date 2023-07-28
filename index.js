164cb43d31c462fcf10b4bd5822f8832b01241aa
tanishq14developer <tanishq.happy@gmail.com>
2023-07-28 23:07:07 +0530

    Feat:updated code

--- index.js
+++ index.js
@@ -3,11 +3,52 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cron = require("node-cron");
+const nodemailer = require("nodemailer");
+const shoppingModel = require("./src/model/shopping.model");
 const route = require("./src/routes/ecomm_routes");
 require("./src/db/db");
 
 const app = express();
 const server = require("http").createServer(app);
+
+async function checkOrdersAndSendEmail() {
+  const now = new Date();
+  const oneHourAgo = new Date(now.getTime() - 60 * 60 * 1000);
+  const ordersPlaced60MinutesAgo = await shoppingModel.find({
+    createdAt: { $gte: oneHourAgo, $lte: now },
+  });
+
+  for (let i = 0; i < ordersPlaced60MinutesAgo.length; i++) {
+    const order = ordersPlaced60MinutesAgo[i];
+
+    const customerEmail = order.email;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.APPPASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: customerEmail,
+      subject: "Order Cart Update",
+      text: "Hey, your order is still waiting in your cart.",
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log("Error sending email:", error);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+  }
+}
 
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -18,6 +59,10 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use(route);
+cron.schedule("* * * * *", () => {
+  console.log("Running cron job...");
+  checkOrdersAndSendEmail();
+});
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`App running on port:: 🎉 ${PORT}`);
